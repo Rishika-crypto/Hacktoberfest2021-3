d8875e45281571bad94577b0e8d62581c87c8c1c
Rishika <72245118+Rishika-crypto@users.noreply.github.com>
2021-10-04 14:54:19 +0530

    Create Insertion_Sort.cpp

---
+++ C++/Insertion_Sort.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+void printArray(int array[], int size){
+    int i;
+    for (i=0; i < size; i++)
+        cout<<array[i]<<" ";
+    cout<<endl;
+}
+
+void insertionSort(int array[], int n){
+    int i, key, j;
+    for (i = 0; i < n; i++){
+        key = array[i];
+        j = i-1;
+        while (j >= 0 && array[j] > key)
+        {                     // find the correct position of the element
+            array[j+1] = array[j];        // shift all lesser elements
+
+            j = j-1;
+        }
+        array[j+1] = key;           // place the element at position
+    }
+}
+
+int main(){
+    int array[] = {15, 11, 14, 12, 18};
+    int n = 5; 
+    /* we can calculate the number of elements in an array by using sizeof(array)/sizeof(array[0]).*/
+    cout<<"Un-Sorted array:"<<endl;
+    printArray(array, n);     // Unsorted array
+    insertionSort(array, n);     // Call the sorting routine
+    cout<<endl<<"Sorted array:"<<endl;
+    printArray(array, n);   // Sorted array
+    return 0;
+}
+
+//Worst and Average Case Time Complexity: O(n^2). Worst case occurs when array is sorted in opposite direction.
+
+//Best Case Time Complexity: O(n). Best case occurs when array is already sorted.
+
+//Auxiliary Space: O(1)
+
+//Sorting In Place: Yes
